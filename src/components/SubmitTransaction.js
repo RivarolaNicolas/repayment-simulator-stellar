cfffcdd9687e314d576525afb093b996fdd13a1b
Martinasd Martinasda <RivarolaNicolas94@gmail.com>
2021-11-28 16:32:50 -0300

    Added working functionality to send payments to the inputted address

--- src/components/SubmitTransaction.js
+++ src/components/SubmitTransaction.js
@@ -1,44 +1,63 @@
+import { useEffect } from 'react';
 const StellarSdk = require('stellar-sdk');
 
 const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
-const borrowerAccount = 'GCE3DHPZUEITQP2B6HHWTQMLIEKR5YYM4HJ3CIVVTETORNCNU3NFNLOT';
 const emitterAccountPublicKey = 'GBJDIEEJ4HUXQSYNINVBAJ35FLSUGFFVLPBHVI65R42GUDKIMJ7PKHGJ';
 const emitterAccountPrivateKey = 'SBJYCFTUCM5GLIZNXRO5T7HTEY2IILMP2GHUTXQMDIAMRCF25U333PCF';
 
-async function main() {
-  const account = await server.loadAccount(emitterAccountPublicKey);
+const SubmitTransaction = (props) => {
+  const borrowerAccount = props.borrowerAccount;
+  const submitTransactionClicked = props.submitTransactionClicked;
+  const setSubmitTransactionClicked = props.setSubmitTransactionClicked;
+  useEffect(() => {
+    if (
+      borrowerAccount.length === 56 &&
+      borrowerAccount.charAt(0) === 'G' &&
+      submitTransactionClicked === true
+    ) {
+      setSubmitTransactionClicked(false);
+      async function SubmitTransactionAsync() {
+        const account = await server.loadAccount(emitterAccountPublicKey);
 
-  /*
+        /*
         Right now, we have one function that fetches the base fee.
         In the future, we'll have functions that are smarter about suggesting fees,
         e.g.: `fetchCheapFee`, `fetchAverageFee`, `fetchPriorityFee`, etc.
     */
-  const fee = await server.fetchBaseFee();
+        const fee = await server.fetchBaseFee();
 
-  const transaction = new StellarSdk.TransactionBuilder(account, {
-    fee,
-    networkPassphrase: StellarSdk.Networks.TESTNET,
-  })
-    .addOperation(
-      // this operation funds the new account with XLM
-      StellarSdk.Operation.payment({
-        destination: borrowerAccount,
-        asset: StellarSdk.Asset.native(),
-        amount: '5',
-      })
-    )
-    .setTimeout(30)
-    .build();
+        const transaction = new StellarSdk.TransactionBuilder(account, {
+          fee,
+          networkPassphrase: StellarSdk.Networks.TESTNET,
+        })
+          .addOperation(
+            // this operation funds the new account with XLM
+            StellarSdk.Operation.payment({
+              destination: borrowerAccount,
+              asset: StellarSdk.Asset.native(),
+              amount: '12',
+            })
+          )
+          .setTimeout(30)
+          .build();
 
-  // sign the transaction
-  transaction.sign(StellarSdk.Keypair.fromSecret(emitterAccountPrivateKey));
+        // sign the transaction
+        transaction.sign(StellarSdk.Keypair.fromSecret(emitterAccountPrivateKey));
 
-  try {
-    const transactionResult = await server.submitTransaction(transaction);
-    console.log(transactionResult);
-  } catch (err) {
-    console.error(err);
-  }
-}
+        try {
+          const transactionResult = await server.submitTransaction(transaction);
+          console.log(transactionResult);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      SubmitTransactionAsync();
+    } else {
+      return null;
+    }
+  }, [borrowerAccount, setSubmitTransactionClicked, submitTransactionClicked]);
 
-export default main;
+  return null;
+};
+
+export default SubmitTransaction;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,50 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import CalculateMinimumRepayment from '../components/CalculateMinimumRepayment';
-import SubmitTransaction from '../components/SubmitTransaction';
-import CreateBorrowerAccount from '../components/CreateBorrowerAccount';
+// import CreateBorrowerAccount from '../components/CreateBorrowerAccount';
 
-const Home = () => {
+const Home = (props) => {
   const [loanAmount, setLoanAmount] = useState(Number);
   const [minimumRepayment, setMinimumRepayment] = useState(Number);
   const [repaymentAmount, setRepaymentAmount] = useState(Number);
-  const [borrowerAccount, setBorrowerAccount] = useState(String);
+  const setSubmitTransactionClicked = props.setSubmitTransactionClicked;
+  const setBorrowerAccount = props.setBorrowerAccount;
 
-  function handleLoanAmount(e) {
+  function handleSetLoanAmount(e) {
     setLoanAmount(e.target.value);
-    console.log(e.target.value, 'loan amount');
   }
 
-  function handleRepaymentAmount(e) {
+  function handleSetRepaymentAmount(e) {
     setRepaymentAmount(e.target.value);
-    console.log(repaymentAmount);
   }
 
-  function handleBorrowerAccount(e) {
+  function handleSetBorrowerAccount(e) {
     setBorrowerAccount(e.target.value);
   }
 
-  function handleCreateBorrowerAccount(e) {
+  function handleSubmitTransactionClicked(e) {
     e.preventDefault();
-    CreateBorrowerAccount();
+    setSubmitTransactionClicked(true);
   }
 
-  function handleSubmitTransaction(e) {
-    e.preventDefault();
-    SubmitTransaction();
-  }
-
+  // function handleSetCreateBorrowerAccount(e) {
+  //   e.preventDefault();
+  //   CreateBorrowerAccount();
+  // }
   return (
     <div>
       <form>
         <h4>Enter loan amount:</h4>
-        <input type="number" onChange={handleLoanAmount}></input>
+        <input type="number" onChange={handleSetLoanAmount}></input>
         <h4>Enter repayment amount:</h4>
         <input
           type="number"
-          onChange={handleRepaymentAmount}
+          onChange={handleSetRepaymentAmount}
           className={repaymentAmount > minimumRepayment ? 'border ' : 'border-2 border-red-500'}
         ></input>
         <h4>Enter the borrower's stellar account:</h4>
-        <input type="text" onChange={handleBorrowerAccount}></input>;
-        <button type="submit" onClick={handleCreateBorrowerAccount}>
+        <input type="text" onChange={handleSetBorrowerAccount}></input>
+        <button type="button" onClick={handleSubmitTransactionClicked}>
           Submit
         </button>
       </form>
