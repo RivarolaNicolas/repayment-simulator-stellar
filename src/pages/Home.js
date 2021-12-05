32f2a83908cb9ee3848ff7cd43685b3abd9ca7b0
Martinasd Martinasda <RivarolaNicolas94@gmail.com>
2021-12-05 00:04:28 -0300

    UI Redesign

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import {
   calculateBorrowerRewards,
 } from '../helpers/calculator';
 import { transactionSubmitter } from '../helpers/stellar';
+import { SiStellar } from 'react-icons/si';
 
 const Home = (props) => {
   const [loanAmount, setLoanAmount] = useState(String);
@@ -69,8 +70,84 @@ const Home = (props) => {
   }
 
   return (
-    <div className="">
-      <form className="">
+    <div className="overflow-x-hidden overflow-y-hidden ">
+      <div className="bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm:overflow-hidden md:my-10 ">
+        <div className="px-4 py-8 sm:px-10">
+          <div className="relative mt-6">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm leading-5">
+              <span className="px-2 text-gray-500 bg-white">Loan Repayment Simulator</span>
+            </div>
+          </div>
+          <div className="mt-6">
+            <div className="w-full space-y-6">
+              <div className="w-full">
+                <div className=" relative ">
+                  <input
+                    type="text"
+                    id="search-form-price"
+                    onChange={handleSetLoanAmount}
+                    className={
+                      'rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+                    }
+                    placeholder="Loan amount"
+                    defaultValue="$"
+                  />
+                </div>
+              </div>
+              <div className="w-full">
+                <div className=" relative ">
+                  <input
+                    type="text"
+                    id="search-form-location"
+                    onChange={handleSetRepaymentAmount}
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Repayment amount"
+                  />
+                </div>
+              </div>
+              <div className="w-full">
+                <div className=" relative ">
+                  <label
+                    forHtml="search-form-name"
+                    className="relative text-gray-400 focus-within:text-gray-600 block"
+                  >
+                    <SiStellar className="pointer-events-none w-4 h-4 absolute top-1/2 transform -translate-y-1/2 left-3" />
+                    <input
+                      name="search-form-name"
+                      type="text"
+                      id="search-form-name"
+                      onChange={handleSetBorrowerAccountPrivateKey}
+                      //className=" rounded-lg border-transparent  appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                      className=" rounded-lg border-transparent border border-gray-300 bg-white placeholder-gray-400 text-gray-700 appearance-none w-full block pl-14 focus:outline-none"
+                      placeholder="Stellar Private Key"
+                    />
+                  </label>
+                </div>
+              </div>
+              <div>
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+                    type="button"
+                    onClick={handleSubmitTransactionClicked}
+                    className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  >
+                    Submit Transaction
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="px-4 py-6 border-t-2 border-gray-200 bg-gray-50 sm:px-10">
+          <p className="text-xs leading-5 text-gray-500">
+            This data are display for information and can change
+          </p>
+        </div>
+      </div>
+      {/* <form className="">
         <h4 className="text-white">Enter loan amount:</h4>
         <input type="number" onChange={handleSetLoanAmount}></input>
         <h4 className="text-white">Enter repayment amount:</h4>
@@ -87,7 +164,7 @@ const Home = (props) => {
           Submit
         </button>
         {transactionSuccessful ? <h1>Transaction successfull</h1> : ''}
-      </form>
+      </form> */}
     </div>
   );
 };
